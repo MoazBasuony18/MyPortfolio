255c2b13eb4276f98faa4481f0071ec855324d91
ETC <ETC@MOAZ>
2021-08-31 14:44:40 +0200

    Add project files.

---
+++ Core/Entites/Owner.cs
@@ -0,0 +1,10 @@
+﻿namespace Core.Entites
+{
+    public class Owner : BaseClass
+    {
+        public string FullName { get; set; }
+        public string  Profil { get; set; }
+        public string Avatar { get; set; }
+        public Address Address { get; set; }
+    }
+}
